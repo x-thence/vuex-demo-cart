08f6104ec1852581fcdf1af65246faa49ad21813
x-thence <2668812066@qq.com>
2021-11-17 19:37:43 +0800

    feat: 初始化页面

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,5 +1,55 @@
 <template>
   <div class="product">
-    <h1>product</h1>
+    <el-button class="btn" type="danger">购物车</el-button>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Product",
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.product {
+  position: relative;
+  width: 75%;
+  margin: 100px auto;
+}
+.btn {
+  position: absolute;
+  right: 50px;
+  z-index: 9;
+}
+</style>
